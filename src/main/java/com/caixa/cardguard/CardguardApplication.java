969c1e1b91c5d7792e6829c0579a07ab89e174b1
Munru <rafaelzoca@gmail.com>
2026-01-27 11:57:32 -0300

    Inicializacao

---
+++ src/main/java/com/caixa/cardguard/CardguardApplication.java
@@ -0,0 +1,13 @@
+package com.caixa.cardguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caixa/cardguard/CardguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caixa.cardguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
